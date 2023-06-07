060ffbfd0912abc7d2a4e6a0ce8a511a44a45c83
omar <oulaya.omar@gmail.com>
2023-06-08 00:29:44 +0200

    sort by date

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ import "./index.scss";
 
 const articleContainerElement = document.querySelector(".articles-container");
 const categoriesContainerElement = document.querySelector(".categories");
+const selectElement = document.querySelector("select");
 let articles;
 let filter;
+let sortBy;
 
 
 const createArticles = ()=>{
     const articlesDOM= articles
+    // Pour le filtre, nous déclarons une variable filter que nous allons utiliser simplement avec la méthode filter() sur notre tableau d'articles lors de la création des éléments articles sur le DOM :
         .filter((article)=>{
+            // Si nous avons un filtre nous retournons le tableau filtré par catégorie, sinon nous retournons le tableau entier en retournant true pour tous les éléments. 
                 if(filter){
                     return article.category === filter
                 }else{
@@ -79,13 +83,15 @@ const createArticles = ()=>{
 
 const fetchArticle = async ()=>{
     try{
-        const response = await fetch ("https://restapi.fr/api/article");
+        // Dans la fonction fetchArticle() nous allons utiliser la valeur de la variable sortBy : 
+        const response = await fetch (`https://restapi.fr/api/article?sort=createdAt:${sortBy}`);
         articles = await response.json();
         // Restapi retourne un objet s'il n'y a qu'un seul article
         // nous devons donc le transformer en tableau :
         if (!Array.isArray(articles)) {
         articles = [articles];
         }
+// nous appelions une nouvelle fonction que nous allons créer lorsque nous récupérons les articles du serveur. 
         createCategory();
         createArticles();
         }
@@ -96,8 +102,13 @@ const fetchArticle = async ()=>{
 }
 fetchArticle();
 
-
+// Puis nous écoutons l'événement change qui est émis dès lors qu'un élément de la liste est sélectionné afin de modifier l'ordre de tri : 
+selectElement.addEventListener('change', ()=>{
+    sortBy = selectElement.value
+    fetchArticle();
+})
 const createCategory = () =>{
+    // es articles récupérées du serveur et va commencer par les transformer en objet avec la méthode reduce() que nous avions vue. 
     const categories = articles.reduce((acc, article)=>{
         if (acc[article.category]){
             acc[article.category]++;
@@ -106,6 +117,7 @@ const createCategory = () =>{
         }
         return acc;
     },{});
+    // Ensuite, nous transformons cet objet en tableau de tableaux contenant en premier index le nom de la catégorie et en second le nombre d'articles dans la catégorie. 
     // sort((a, b) => a.localeCompare(b)) => pour trier en ordre alphabetique
     const categoryArr = Object.keys(categories).sort((a, b) => a.localeCompare(b)).map(category=>{
         return [category, categories[category]];
@@ -113,10 +125,17 @@ const createCategory = () =>{
     displayMenuCategoris(categoryArr);
 }
 
+// Cette fonction va créer un élément de liste pour chaque catégorie contenu dans le tableau.
+// Chaque élément de liste contiendra le nom de la catégorie et le nombre d'articles dans la catégorie
 const displayMenuCategoris = (categoryArr) => {
     const elements = categoryArr.map(element=>{
         const lis = document.createElement('li');
         lis.innerHTML = `${element[0]} (<strong>${element[1]}</strong>)`;
+        // Pour un tri par date - Il ne nous reste plus qu'à ajouter la classe active sur la catégorie sélectionnée si il existe un filtre, lors de la création et de l'affichage du menu des catégories : 
+        if (element[0] === filter) {
+            lis.classList.add("active");
+          }
+        //   pour filter
         lis.addEventListener('click',()=>{
             if(filter === element[0]){
                 elements.forEach( li=>{
@@ -136,7 +155,7 @@ const displayMenuCategoris = (categoryArr) => {
         })
         return lis;
     });
-
+// Enfin, nous n'avons qu'à append() les éléments sur le conteneur ayant la classe categories et sur lequel nous avons créé une référence. 
     categoriesContainerElement.innerHTML = '';
     categoriesContainerElement.append(...elements);
 }
